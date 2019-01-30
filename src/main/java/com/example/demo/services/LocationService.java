6e6665072cff3dcaaeb0c30a61798f3ad9ff037b
Saurin Shah <shah.saurin@husky.neu.edu>
2019-01-29 19:09:10 -0500

    created basic apis

--- src/main/java/com/example/demo/services/LocationService.java
+++ src/main/java/com/example/demo/services/LocationService.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,5 +19,81 @@ public class LocationService {
     @GetMapping("/api/location/all")
     public List<Location> findAllLocationBasedData() {
         return (List<Location>) locationRepository.findAll();
+    }
+
+    @GetMapping("/api/level=state/all")
+    public List<Location> findAllStates() {
+        List<Location> locationList = (List<Location>) locationRepository.findAll();
+        List<Location> stateList = new ArrayList<>();
+        for(Location location: locationList){
+            if (location.getType_code()  == 0){
+                stateList.add(location);
+            }
+        }
+        return stateList;
+    }
+
+    @GetMapping("/api/level=county/all")
+    public List<Location> findAllCounties() {
+        List<Location> locationList = (List<Location>) locationRepository.findAll();
+        List<Location> countyList = new ArrayList<>();
+        for(Location location: locationList){
+            if (location.getType_code()  == 1){
+                countyList.add(location);
+            }
+        }
+        return countyList;
+
+    }
+
+    @GetMapping("/api/level=city/all")
+    public List<Location> findAllCities() {
+        List<Location> locationList = (List<Location>) locationRepository.findAll();
+        List<Location> cityList = new ArrayList<>();
+        for(Location location: locationList){
+            if (location.getType_code()  == 2){
+                cityList.add(location);
+            }
+        }
+        return cityList;
+
+    }
+
+    @GetMapping("/api/level=town/all")
+    public List<Location> findAllTowns() {
+        List<Location> locationList = (List<Location>) locationRepository.findAll();
+        List<Location> townList = new ArrayList<>();
+        for(Location location: locationList){
+            if (location.getType_code()  == 3){
+                townList.add(location);
+            }
+        }
+        return townList;
+
+    }
+
+    @GetMapping("/api/level=district/all")
+    public List<Location> findAllDistricts() {
+        List<Location> locationList = (List<Location>) locationRepository.findAll();
+        List<Location> districtList = new ArrayList<>();
+        for(Location location: locationList){
+            if (location.getType_code()  == 4){
+                districtList.add(location);
+            }
+        }
+        return districtList;
+
+    }
+
+    @GetMapping("/api/level=other/all")
+    public List<Location> findAllOtherLocations() {
+        List<Location> locationList = (List<Location>) locationRepository.findAll();
+        List<Location> otherList = new ArrayList<>();
+        for(Location location: locationList){
+            if (location.getType_code()  == 5){
+                otherList.add(location);
+            }
+        }
+        return otherList;
     }
 }
