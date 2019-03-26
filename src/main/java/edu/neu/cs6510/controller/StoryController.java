42327aba7b847041682bb6e40eaf0ce73bdec41c
Xiao Han <han.xiao1@husky.neu.edu>
2019-03-26 13:31:39 -0700

    minor code optimized

--- src/main/java/edu/neu/cs6510/controller/StoryController.java
+++ src/main/java/edu/neu/cs6510/controller/StoryController.java
@@ -3,11 +3,14 @@ package edu.neu.cs6510.controller;
 import edu.neu.cs6510.config.ElasticsearchConfig;
 import edu.neu.cs6510.model.Story;
 import edu.neu.cs6510.services.StoryService;
+import edu.neu.cs6510.util.http.ResponseMessage;
+import edu.neu.cs6510.util.http.Result;
 import io.searchbox.client.JestClient;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.ws.Response;
 import java.util.List;
 
 @RestController
@@ -22,48 +25,47 @@ public class StoryController {
 
 
     @ApiOperation(value = "find all stories")
-    @GetMapping("/stories/story/all")
-    public List<Story> findAllStories() {
-        return storyService.getAll();
+    @GetMapping("/api/stories/story/all")
+    public ResponseMessage<List<Story>> findAllStories() {
+        return Result.success(storyService.getAll());
     }
 
 
     @ApiOperation(value = "find story by id")
-    @GetMapping("/stories/story/{id}")
-    public List<Story> findStoryById(@PathVariable(value = "id") String id) {
-        return storyService.getById(id);
+    @GetMapping("/api/stories/story/{id}")
+    public ResponseMessage<List<Story>> findStoryById(@PathVariable(value = "id") String id) {
+        return Result.success(storyService.getById(id));
     }
 
 
     @ApiOperation(value = "add story")
-    @PostMapping("/stories/story/add")
-    public Story addStory(@RequestBody Story story) {
-        return storyService.createStory(story);
+    @PostMapping("/api/stories/story/add")
+    public ResponseMessage<Story> addStory(@RequestBody Story story) {
+        return Result.success(storyService.createStory(story));
     }
 
     @ApiOperation(value = "update story")
-    @PutMapping("/stories/story/update")
-    public Story updateStory(@RequestBody Story story) {
-        ElasticsearchConfig client = new ElasticsearchConfig();
-        return storyService.updateStory(story);
+    @PutMapping("/api/stories/story/update")
+    public ResponseMessage<Story> updateStory(@RequestBody Story story) {
+        return Result.success(storyService.updateStory(story));
     }
 
     @ApiOperation(value = "find story by id")
-    @GetMapping("/stories/story/all/sortBy={fieldName}")
-    public List<Story> orderStoryByField(@PathVariable(value = "fieldName") String fieldName, @RequestParam(value = "orderType", required = false) String orderType) {
-        return storyService.searchByFieldandOrder(fieldName, orderType);
+    @GetMapping("/api/stories/story/all/sortBy={fieldName}")
+    public ResponseMessage<List<Story>> orderStoryByField(@PathVariable(value = "fieldName") String fieldName, @RequestParam(value = "orderType", required = false) String orderType) {
+        return Result.success(storyService.searchByFieldandOrder(fieldName, orderType));
     }
 
     @ApiOperation(value = "update story upvote or downvote")
-    @PutMapping("/stories/story/{id}/update/vote")
-    public List<Story> updateStoryVotes(@PathVariable(value = "id") String id, @RequestParam(value = "voteType") String voteType, @RequestParam(value = "value") int value){
-        return storyService.updateVote(client, id, voteType, value);
+    @PutMapping("/api/stories/story/{id}/update/vote")
+    public ResponseMessage<List<Story>> updateStoryVotes(@PathVariable(value = "id") String id, @RequestParam(value = "voteType") String voteType, @RequestParam(value = "value") int value){
+        return Result.success(storyService.updateVote(client, id, voteType, value));
     }
 
     @ApiOperation(value = "update story upvote or downvote")
-    @GetMapping("/stories/story/search/{keyword}")
-    public List<Story> updateStoryVotes(@PathVariable(value = "keyword") String keyword){
-        return storyService.searchByKeyword(client, keyword);
+    @GetMapping("/api/stories/story/search/{keyword}")
+    public ResponseMessage<List<Story>> updateStoryVotes(@PathVariable(value = "keyword") String keyword){
+        return Result.success(storyService.searchByKeyword(client, keyword));
     }
 
 }
